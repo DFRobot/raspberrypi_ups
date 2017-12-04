d18bb92ff15dfc6f6c12dd9fcbb7709aadc65bf4
mengbishu <30721964+mengbishu@users.noreply.github.com>
2017-12-04 15:47:16 +0800

    Update ups_wdog.c

--- ups_wdog.c
+++ ups_wdog.c
@@ -1,11 +1,17 @@
 #include <wiringPiI2C.h>
 #include <unistd.h>
+#include <signal.h>
 #define ID 0x18
-void main(){
-	int fd = wiringPiI2CSetup(0x18);
-	while(1){
-		wiringPiI2CWriteReg8(fd,0x0f,0x14);
-		sleep(5);
-	}
+int fd;
+void fun(int signal_num){
+    wiringPiI2CWriteReg8(fd,0x09,0x2);
+}
 
+void main(){
+    fd = wiringPiI2CSetup(0x18);
+    signal(SIGTERM,fun);
+    while(1){
+        wiringPiI2CWriteReg8(fd,0x0f,0x14);
+        sleep(5);
+    }
 }
